169d0013348bdab5e1fd96df8209762645eee2e1
morgancook <jmcook@wm.edu>
2025-04-20 21:54:17 -0400

    added createAndShowGUI() and shutdown() methods to allow for the creation and distruction of main window

--- src/main/java/edu/wm/cs/cs301/sudoku/Main.java
+++ src/main/java/edu/wm/cs/cs301/sudoku/Main.java
@@ -1,7 +1,9 @@
 package edu.wm.cs.cs301.sudoku;
 
+import edu.wm.cs.cs301.sudoku.view.SudokuFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        new SudokuFrame();
     }
 }
--- src/main/java/edu/wm/cs/cs301/sudoku/view/SudokuFrame.java
+++ src/main/java/edu/wm/cs/cs301/sudoku/view/SudokuFrame.java
@@ -1,4 +1,36 @@
 package edu.wm.cs.cs301.sudoku.view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class SudokuFrame {
+    private JFrame frame;
+
+    public SudokuFrame() {
+        this.frame = createAndShowGUI();
+    }
+
+    private JFrame createAndShowGUI() {
+        JFrame frame = new JFrame("Sudoku");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setResizable(false);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                shutdown();
+            }
+        });
+
+        frame.pack();
+        frame.setVisible(true);
+
+        return frame;
+    }
+
+    public void shutdown() {
+        frame.dispose();
+        System.exit(0);
+    }
 }
