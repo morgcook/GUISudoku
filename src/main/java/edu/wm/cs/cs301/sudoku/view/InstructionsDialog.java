8956b4c74c090e166e6641504d41e6ade5d71c7e
morgancook <jmcook@wm.edu>
2025-04-20 22:16:36 -0400

    added menu bar and foundation for InstructionsDialog

--- src/main/java/edu/wm/cs/cs301/sudoku/view/InstructionsDialog.java
+++ src/main/java/edu/wm/cs/cs301/sudoku/view/InstructionsDialog.java
@@ -1,4 +1,16 @@
 package edu.wm.cs.cs301.sudoku.view;
 
-public class InstructionsDialog {
+import javax.swing.*;
+import java.awt.*;
+
+public class InstructionsDialog extends JDialog {
+    private SudokuFrame view;
+
+    public InstructionsDialog(SudokuFrame view) {
+        super(view.getFrame(), "Instructions", true);
+
+        pack();
+        setLocationRelativeTo(view.getFrame());
+        setVisible(true);
+    }
 }
--- src/main/java/edu/wm/cs/cs301/sudoku/view/SudokuFrame.java
+++ src/main/java/edu/wm/cs/cs301/sudoku/view/SudokuFrame.java
@@ -2,6 +2,8 @@ package edu.wm.cs.cs301.sudoku.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -23,14 +25,38 @@ public class SudokuFrame {
             }
         });
 
+        frame.setJMenuBar(createMenuBar());
+
         frame.pack();
         frame.setVisible(true);
 
         return frame;
     }
 
+    private JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenuItem instructionsItem = new JMenuItem("Instructions");
+        instructionsItem.addActionListener(event -> new InstructionsDialog(this));
+        menuBar.add(instructionsItem);
+
+        JMenuItem newGameItem = new JMenuItem("New Game");
+        //newGameItem.addActionListener(); future implementation
+        menuBar.add(newGameItem);
+
+        JMenuItem quitItem = new JMenuItem("Quit");
+        quitItem.addActionListener(event -> shutdown());
+        menuBar.add(quitItem);
+
+        return menuBar;
+    }
+
     public void shutdown() {
         frame.dispose();
         System.exit(0);
+    }
+
+    public JFrame getFrame() {
+        return this.frame;
     }
 }
