a3ce0ac623ede3116d6c31c380758b3df6293491
morgancook <jmcook@wm.edu>
2025-04-25 17:18:59 -0400

    changed SudokuModel to SudokuPuzzle and transferred over the code from the original sudoku project

---
+++ src/main/java/edu/wm/cs/cs301/sudoku/model/SudokuPuzzle.java
@@ -0,0 +1,219 @@
+package edu.wm.cs.cs301.sudoku.model;
+
+import edu.wm.cs.cs301.sudoku.view.SudokuFrame;
+
+import java.util.*;
+
+public class SudokuPuzzle {
+    public static final int NUM_ROWS = 9;
+    public static final int NUM_COLS = 9;
+
+    // the original version of the puzzle excluding valid player moves
+    private final int[][] original = new int[NUM_ROWS][NUM_COLS];
+    // the current version of the puzzle including valid player moves
+    private final int[][] current = new int[NUM_ROWS][NUM_COLS];
+    // the solution to the current puzzle
+    private final int[][] solution = new int[NUM_ROWS][NUM_COLS];
+
+    // Hash Map that maps row/column labels to their respective indices
+    private final Map<Character, Integer> map = new HashMap<>();
+
+    private int counter = 0;
+    private int attempts = 1;
+
+    public SudokuPuzzle(SudokuFrame view) {
+        // Creates lookup table for labels to indices
+        String chars = "abcdefghi";
+        for (int i = 0; i < chars.length(); i++) {
+            map.put(chars.charAt(i), i);
+        }
+
+        fillGrid(solution);
+        deepCopy2D(solution, original);
+        createPuzzle();
+        deepCopy2D(original, current);
+    }
+
+    public int[][] getCurrent() {
+        return current;
+    }
+
+    public int[][] getSolution() {
+        return solution;
+    }
+
+    // prints the 2d array of the inputted board, with the option of colored user-inputs
+    public void printPuzzle(int[][] board, boolean color) {
+        char[] rows = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+        String hline = "  +-----------------------------+";
+        String vline = "  |         |         |         |";
+
+        // adds column labels
+        System.out.println("    A  B  C   D  E  F   G  H  I");
+
+
+        for (int i = 0; i < 9; i++) {
+            // separates every third item with a horizontal line
+            if (i % 3 == 0) {
+                System.out.println(hline);
+                System.out.println(vline);
+            }
+
+            // adds row labels
+            System.out.print(rows[i]);
+            for (int j = 0; j < 9; j++) {
+                // separates every third column with a vertical line
+                if (j % 3 == 0) {
+                    System.out.print(" | ");
+                } else {
+                    System.out.print("  ");
+                }
+                if (board[i][j] == 0) {
+                    System.out.print(" ");
+                } else {
+                    // if color print mode is true then user moves are highlighted
+                    if (board[i][j] == original[i][j] || !color) {
+                        System.out.print(board[i][j]);
+                    } else {
+                        System.out.print("\u001B[47m\u001B[30m" + board[i][j] + "\u001B[0m");
+                    }
+                }
+            }
+            System.out.println(" |");
+            System.out.println(vline);
+        }
+
+        System.out.println(hline);
+    }
+
+    // following three functions based on given pseudocode
+    private boolean fillGrid(int[][] board) {
+        Integer[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                if (board[row][col] == 0) {
+                    Collections.shuffle(Arrays.asList(values));
+                    for (int val : values) {
+                        if (isSafe(board, row, col, val)) {
+                            board[row][col] = val;
+                            if (fillGrid(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean solveGrid(int[][] board) {
+        Integer[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                if (board[row][col] == 0) {
+                    Collections.shuffle(Arrays.asList(values));
+                    for (int val : values) {
+                        if (isSafe(board, row, col, val)) {
+                            board[row][col] = val;
+                            if (solveGrid(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        counter++;
+        return false;
+    }
+
+    private void createPuzzle() {
+        int row, col;
+        Random random = new Random();
+        while (attempts > 0) {
+            do {
+                row = random.nextInt(NUM_ROWS);
+                col = random.nextInt(NUM_COLS);
+            } while (original[row][col] == 0);
+
+            int backup = original[row][col];
+            original[row][col] = 0;
+
+            int[][] copyGrid = new int[NUM_ROWS][NUM_COLS];
+            for (int i = 0; i < NUM_ROWS; i++) {
+                System.arraycopy(original[i], 0, copyGrid[i], 0, original[0].length);
+            }
+            counter = 0;
+            solveGrid(copyGrid);
+            if (counter != 1) {
+                original[row][col] = backup;
+                attempts--;
+            }
+        }
+    }
+
+    // deep copies the contents of a 2d array to another
+    private void deepCopy2D(int[][] source, int[][] destination) {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            System.arraycopy(source[i], 0, destination[i], 0, source[0].length);
+        }
+    }
+
+    // function for the execution of valid moves
+    public boolean makeMove(String input) {
+        String[] values = input.split(" ");
+
+        // check if input is of "x x x" format
+        if (input.length() != 5 || values.length != 3) {
+            return false;
+        }
+
+        // get row and column index from character input
+        int row = map.getOrDefault(values[0].toLowerCase().charAt(0), -1);
+        int col = map.getOrDefault(values[1].toLowerCase().charAt(0), -1);
+
+        // checks if number input is an integer
+        if (!Character.isDigit(values[2].charAt(0))) {
+            return false;
+        }
+
+        int num = Integer.parseInt(values[2]);
+
+        // checks if the move is valid by sudoku rules and is not overwriting an original square
+        //  and updates current board if it is a valid move
+        if (original[row][col] == 0 && (num == 0 || isSafe(current, row, col, num))) {
+            current[row][col] = num;
+            return true;
+        }
+
+        return false;
+    }
+
+    // checks if move is valid by sudoku rules and by making sure inputs are within the correct bounds
+    public static boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int x = 0; x < NUM_ROWS; x++) {
+            if (row == -1 || col == -1 || num < 0 || num > 9 || board[row][x] == num || board[x][col] == num
+                    || board[row - row % 3 + x / 3][col - col % 3 + x % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // checks if board is full (completed)
+    public boolean isFull(int[][] board) {
+        for (int[] row : board) {
+            for (int val : row) {
+                if (val == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
