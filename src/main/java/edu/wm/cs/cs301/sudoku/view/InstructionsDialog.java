066267fc7b82050ed8bb0d2dae5c302cab30c639
morgancook <jmcook@wm.edu>
2025-04-27 00:45:11 -0400

    created InstructionsDialog using new instructions.html file, derived from previous Wordle project

--- src/main/java/edu/wm/cs/cs301/sudoku/view/InstructionsDialog.java
+++ src/main/java/edu/wm/cs/cs301/sudoku/view/InstructionsDialog.java
@@ -2,15 +2,48 @@ package edu.wm.cs.cs301.sudoku.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class InstructionsDialog extends JDialog {
-    private SudokuFrame view;
-
     public InstructionsDialog(SudokuFrame view) {
         super(view.getFrame(), "Instructions", true);
+
+        add(createMainPanel(), BorderLayout.CENTER);
+        add(createButtonPanel(), BorderLayout.SOUTH);
 
         pack();
         setLocationRelativeTo(view.getFrame());
         setVisible(true);
+    }
+
+    private JPanel createMainPanel() {
+        JPanel panel = new JPanel();
+
+        URL url = InstructionsDialog.class.getResource("/instructions.html");
+
+        JEditorPane editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+        editorPane.setContentType("text/html");
+        try {
+            editorPane.setPage(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JScrollPane scrollPane = new JScrollPane(editorPane);
+        scrollPane.setPreferredSize(new Dimension(400, 400));
+        panel.add(scrollPane);
+
+        return panel;
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Cancel");
+        button.addActionListener(e -> dispose());
+
+        panel.add(button);
+        return panel;
     }
 }
